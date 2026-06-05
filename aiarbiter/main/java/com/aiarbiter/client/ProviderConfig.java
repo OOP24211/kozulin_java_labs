001a8bdd5fe52255bf4edcb242de6978581ca76c
g.kozulin <g.kozulin@g.nsu.ru>
2026-06-05 13:17:56 +0700

    Удалил мусор

--- aiarbiter/main/java/com/aiarbiter/client/ProviderConfig.java
+++
@@ -1,7 +0,0 @@
-package com.aiarbiter.client;
-
-public record ProviderConfig(
-        String baseUrl,
-        String apiKey,
-        String model
-) {}
--- aiarbiter/main/java/com/aiarbiter/model/auth/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.aiarbiter.model.auth;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank String username,
-        @NotBlank String password
-) {}
